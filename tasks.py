f8d15d6c6a22b2b493a215507bffe69838d9a7a4
koki.chin <545705507@qq.com>
2024-07-26 16:22:38 +0900

    try as api

--- tasks.py
+++ tasks.py
@@ -1,71 +1,96 @@
 
-from celery import Celery
-from time import sleep
-
-from fastapi import FastAPI
-from celery.result import AsyncResult
+from fastapi import FastAPI, HTTPException
+from Manager import MT5Manager,MT5Account,MT5Action,Book
+class GetBooks(MT5Action):
+    def run(acc:MT5Account):
+        tbs = {f'{b.symbol}-{b.price_open}-{b.volume}':b.model_dump() for b in Book().getBooks()}
+        return tbs
 
 ######################################### Celery connect to local rabbitmq and db sqlite backend
-app = Celery('tasks', broker = 'amqp://localhost', backend = 'db+sqlite:///db.sqlite3')
 
-@app.task #register task into celery app via decorator
-def get_hello(name):
-    sleep(10) #simulation for long running task
-    retval = f"Hello {name}"
-    return retval
+from celery import Celery
+from celery.app import task as Task
+celery_app = Celery('tasks', broker = 'amqp://localhost', backend = 'db+sqlite:///db.sqlite3')     
+mt5manager = MT5Manager()
+# mt5manager.add_terminal()    
+class CeleryTask:
+    
+    @staticmethod
+    @celery_app.task(bind=True)
+    def revoke(t:Task, task_id: str):
+        """Method to revoke a task."""
+        return CeleryTask.celery_app.control.revoke(task_id, terminate=True)
 
-@app.task
-def book_send(self):
-    pass
+    @staticmethod
+    @celery_app.task(bind=True)
+    def get_books(t:Task, acc:MT5Account):
+        gtb = GetBooks(acc)  
+        mt5manager.do(gtb)
+        return mt5manager.results[gtb.uuid][0]
 
-@app.task
-def book_close(self):
-    pass
+    @staticmethod
+    @celery_app.task(bind=True)
+    def book_send(t:Task,acc:MT5Account):
+        pass
 
-@app.task
-def book_changeP(self,p):
-    pass
+    @staticmethod
+    @celery_app.task(bind=True)
+    def book_close(t:Task,acc:MT5Account):
+        pass
 
-@app.task
-def book_changeTS(self,tp,sl):
-    pass
+    @staticmethod
+    @celery_app.task(bind=True)
+    def book_changeP(t:Task,acc:MT5Account,p):
+        pass
 
-@app.task
-def getBooks(self):
-    pass
+    @staticmethod
+    @celery_app.task(bind=True)
+    def book_changeTS(t:Task,acc:MT5Account,tp,sl):
+        pass
 
-@app.task
-def account_info(self):
-    pass
+    @staticmethod
+    @celery_app.task(bind=True)
+    def account_info(t:Task,acc:MT5Account):
+        pass
 
 
 ######################################### Create FastAPI app instance
-api = FastAPI()
 
-@api.get("/books/{name}")
-def book_send(self):
-    pass
+class RESTapi:   
+    api = FastAPI()
 
-@api.get("/books/{name}")
-def book_close(self):
-    pass
+    @staticmethod
+    @api.post("/books/")
+    def get_books(acc:MT5Account):
+        task = CeleryTask.get_books.delay(acc)
+        return {'id':task.id}
 
-@api.get("/books/{name}")
-def book_changeP(self,p):
-    pass
-
-@api.get("/books/{name}")
-def book_changeTS(self,tp,sl):
-    pass
-
-@api.get("/books/")
-def getBooks(self):
-    pass
-
-@api.get("/accounts/")
-def account_info(self):
-    pass
-
-@api.get("/accounts/{id}")
-def account_info(self):
-    pass
+    @staticmethod
+    @api.get("/books/{name}")
+    def book_send(acc:MT5Account):
+        task = CeleryTask.book_send.delay(acc)
+        return {'id':task.id}
+    
+    @staticmethod
+    @api.get("/books/{name}")
+    def book_close(acc:MT5Account):
+        task = CeleryTask.book_close.delay(acc)
+        return {'id':task.id}
+    
+    @staticmethod
+    @api.get("/books/{name}")
+    def book_changeP(acc:MT5Account,p):
+        task = CeleryTask.book_changeP.delay(acc,p)
+        return {'id':task.id}
+    
+    @staticmethod
+    @api.get("/books/{name}")
+    def book_changeTS(acc:MT5Account,tp,sl):
+        task = CeleryTask.book_changeTS.delay(acc,tp,sl)
+        return {'id':task.id}
+    
+    @staticmethod
+    @api.get("/accounts/")
+    def account_info(acc:MT5Account):
+        task = CeleryTask.account_info.delay(acc)
+        return {'id':task.id}
